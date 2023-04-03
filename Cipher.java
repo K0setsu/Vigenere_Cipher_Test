1a32a1a85e4cfd5b711a8d4ee291794f703435c8
K0setsu <123960214+K0setsu@users.noreply.github.com>
2023-04-03 08:39:31 +0300

    Add files via upload
    - modified code to be able to accept new key to cipher with, it can be changed with start of the program.

--- Cipher.java
+++ Cipher.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -13,53 +14,79 @@ import java.util.Scanner;
 public class Cipher {
     public static void main(String[] args) throws IOException {
         boolean again = true;
+        //String key = new String("BAAAAB");
 
-        String key = "CLANG"; //key
-        do {
-        Scanner toDo = new Scanner(System.in);
-        System.out.println("Encrypt new/Decrypt old?");
-        String doing = toDo.nextLine();
+        Scanner keyChange = new Scanner(System.in);
+        System.out.println("Do you want to change the key?");
+        String changeKey = keyChange.nextLine();
 
+            switch (changeKey) {
+                case ("Yes"):
+                    Scanner makeKey = new Scanner(System.in);
+                    File newKey = new File("Key.txt");
+                    System.out.println("Please enter new key:");
+                    String fileKey = makeKey.nextLine();
+                    FileWriter nk = new FileWriter(newKey);
+                    nk.write(fileKey);
+                    nk.close();
 
+                    break;
+                case ("No"):
+                    break;
+                default:
+                    break;
 
-    switch (doing) {
-        case ("Encrypt"):
-            Scanner text = new Scanner(System.in);
-            System.out.println("Type the messege in:");
-            String originalMessege = text.nextLine();
-            File file = new File("OriginalText.txt");
-            File cryptedFile = new File("Crypt.txt");
-            FileWriter pw = new FileWriter(file);
-            pw.write(originalMessege);
-            pw.close();
+            }
+            String key = new String(Files.readAllBytes(Paths.get("Key.txt")));
+            do {
+                Scanner toDo = new Scanner(System.in);
+                System.out.println("Encrypt new/Decrypt old?");
+                String doing = toDo.nextLine();
 
-            String messege = originalMessege;
-            String encryptedMessege = encrypt(messege, key); //encrypting and saving
-            FileWriter crypto = new FileWriter(cryptedFile);
-            crypto.write(encryptedMessege);
-            crypto.close();
+                switch (doing) {
 
-            Scanner write_out = new Scanner(cryptedFile); //messege out
-            while (write_out.hasNextLine())
-                System.out.println(write_out.nextLine());
-            break;
-        case ("Decrypt"):
-            File crypted = new File("Crypt.txt");
-            String messegeFromFile = new String(Files.readAllBytes(Paths.get("Crypt.txt")));
-            String decypher = Decrypt(messegeFromFile, key);
-            System.out.println(messegeFromFile);
-            System.out.println("Original messege is:" + decypher);
-            break;
-        case ("Exit") : System.exit(0);
-        break;
-        default:
-            System.out.println("try again");
-            again = false;
-            break;
-    }
-}while(again = true);
+                    case ("Encrypt"):
+                        Scanner text = new Scanner(System.in);
+                        System.out.println("Type the messege in:");
+                        String originalMessege = text.nextLine();
+                        File file = new File("OriginalText.txt");
+                        File cryptedFile = new File("Crypt.txt");
+                        FileWriter pw = new FileWriter(file);
+                        pw.write(originalMessege);
+                        pw.close();
+
+                        String messege = originalMessege;
+                        String encryptedMessege = encrypt(messege, key); //encrypting and saving
+                        FileWriter crypto = new FileWriter(cryptedFile);
+                        crypto.write(encryptedMessege);
+                        crypto.close();
+
+                        Scanner write_out = new Scanner(cryptedFile); //messege out
+                        while (write_out.hasNextLine())
+                            System.out.println(write_out.nextLine());
+                        again = false;
+                        break;
+                    case ("Decrypt"):
+                        File crypted = new File("Crypt.txt");
+                        String messegeFromFile = new String(Files.readAllBytes(Paths.get("Crypt.txt")));
+                        String decypher = Decrypt(messegeFromFile, key);
+                        System.out.println(messegeFromFile);
+                        System.out.println("Original messege is:" + decypher);
+                        again = false;
+                        break;
+                    case ("Exit"):
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("try again");
+                        again = true;
+                        break;
+                }
+            } while (again == true);
 
         }
+
+
 
 //encryption
         public static String encrypt (String text, final String key){
